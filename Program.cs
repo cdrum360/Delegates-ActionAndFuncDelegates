d1a5f311238990dc3f2f678a81bb5778c65a091d
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 13:15:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ActionAndFuncDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            // Use the Action<> delegate to point to DisplayMessage. 
+            Action<string, ConsoleColor, int> actionTarget =  new Action<string, ConsoleColor, int>(DisplayMessage);
+            actionTarget("Action Message!", ConsoleColor.Yellow, 5);
+
+            //Use the Func<> delegate to point to SumToString
+            var funcTarget2 = new Func<int, int, string>(SumToString);     // used implicit typing; var keyword
+            string sum = funcTarget2(90, 270);
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+        }
+
+        // This is a target for the Action<> delegate. 
+        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
+        {
+            // Set color of console text. 
+            ConsoleColor previous = Console.ForegroundColor;
+            Console.ForegroundColor = txtColor;
+
+            for (int i = 0; i < printCount; i++)
+            {
+                Console.WriteLine(msg);
+            }
+
+            // Restore color. 
+            Console.ForegroundColor = previous;
+        }
+
+        // Targets for the Func<> delegate. 
+        static string SumToString(int x, int y)
+        {
+            return (x + y).ToString();
+        }
+
+    }
+}
